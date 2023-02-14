368fdba01658993b9e01e2dd3022f28009e8488a
Douglas Ramírez <ra.douglas@hotmail.com>
2023-02-14 18:51:01 -0500

    Base template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import "./scss/style.scss";
 
 function App() {
+  const fullPage = {
+    minHeight: "100vh"
+  }
   return (
     <div className="App">
-      <h1 className='text-center text-primary'>Holas</h1>
+      <div className="container-fluid">
+        <div className="row" style={fullPage}>
+          <div className="col-4 bg-dark"></div>
+          <div className="col-8"></div>
+        </div>
+      </div>
     </div>
   );
 }
